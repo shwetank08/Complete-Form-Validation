6412a425ad78340800f570d7c5afd4b7391e7b4e
shwetank08 <shwetanksingh.in@gmail.com>
2022-10-14 13:11:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const email = document.querySelector("#email");
+const userName = document.querySelector("#username");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+const btn = document.querySelector("button");
+const msg = document.querySelector('.msg');
+
+
+function display(e){
+    msg.innerText = "";
+    let message = document.createElement('h1');
+    message.innerText = e.toUpperCase();
+    msg.appendChild(message)
+}
+
+function isValidEmail(e){
+    const reg = /[A-Za-z._]{1,}@[A-Za-z]{3,}[.]{1}[a-zA-Z.]{3,}/;
+    if(reg.test(e)){
+        return true;
+    }
+    return false;
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(!isValidEmail(email.value)){
+    display("Please enter valid mail id");
+    return;
+  }
+  if(email.value === ""||userName.value === ""||password.value === ""||confirmPassword.value === ""){display("Please input all the fields"); return;}
+
+  if(password.value === confirmPassword.value){
+    display("User signed up")
+  }else{
+    display("Password don't match")
+  }
+  email.value = "";
+  userName.value = "";
+  password.value = "";
+  confirmPassword.value = "";
+});
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Complete Form Validation
+
+## Description
+
+a simple signup form with four input fields: username, 
+email, password, confirm password and when you click the sign up 
+without filling anything or incorrect data, the form will show an error message.
+
+## Tech stack
+    - Javascript
+    - HTML
+    - CSS
+
+## Deployed Link
+
+[Link]()
+
+## Screenshots
+
+![Img](./Images/Form.png)
